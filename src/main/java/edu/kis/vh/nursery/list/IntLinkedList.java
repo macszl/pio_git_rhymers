4650c2bee0f3017f64f87869b6c24ee268e75328
macszl <235989@edu.p.lodz.pl>
2022-05-09 13:00:28 +0200

    13. Usuniecie pliku Node.

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -45,3 +45,26 @@ class IntLinkedList {
 
 
 }
+
+class Node {
+
+
+    public final int value;
+    public Node prev, next;
+
+    public Node(int i) {
+        value = i;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+}
