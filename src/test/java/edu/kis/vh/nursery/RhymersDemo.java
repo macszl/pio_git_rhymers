81a77ba004e907668f2a03c47c7afed10bd2f0be
macszl <235989@edu.p.lodz.pl>
2022-05-09 12:57:53 +0200

    12. Ekstracja metod z demo RhymersDemo

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -11,28 +11,32 @@ class RhymersDemo {
 
 	public static void main(String[] args) {
 		RhymersFactory factory = new DefaultRhymersFactory();
-		
+
+		testRhymers(factory);
+
+	}
+
+	private static void testRhymers(RhymersFactory factory) {
 		DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
 				factory.getFIFORhymer(), factory.getHanoiRhymer()};
 
 		for (int i = 1; i < TEST_VAL; i++)
 			for (int j = 0; j < rhymers.length - 1; j++)
 				rhymers[j].countIn(i);
-		
+
 		java.util.Random rn = new java.util.Random();
 		for (int i = 1; i < TEST_VAL; i++)
 			rhymers[HANOI_RHYMER_IDX].countIn(rn.nextInt(RAND_UPPER_BOUND));
-		
+
 		for (int i = 0; i < rhymers.length; i++) {
 			while (!rhymers[i].callCheck())
 				System.out.print(rhymers[i].countOut() + "  ");
 			System.out.println();
 		}
-		
+
 		System.out.println("total rejected is "
 				+ ((HanoiRhymer) rhymers[HANOI_RHYMER_IDX]).reportRejected());
-		
 	}
-	
+
 
 }
