21dadac0b7975310cc913f15fad547fdc0837d39
macszl <235989@edu.p.lodz.pl>
2022-05-09 13:10:27 +0200

    16. Opisanie klasy IntLinkedList.

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,5 +1,9 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * The IntLinkedList class is an implementation of the common data structure: one-way linked list.
+ * This class contains a Node class, and functions allowing adding, deleting and checking the last Node element.
+ */
 class IntLinkedList {
 
 
@@ -7,6 +11,12 @@ class IntLinkedList {
     private Node last;
     int i;
 
+    /**
+     * @param i
+     * This function creates a new Node object with the value of i, and places it at the end of the linked list.
+     * If the list is empty, this just involves the invocation of the Node constructor.
+     * If not, the function also updates the prev and next of the previous last node.
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -25,12 +35,22 @@ class IntLinkedList {
         return false;
     }
 
+    /**
+     * @return
+     * This functon returns the value of the last Node object of the linked list.
+     * Returns ERROR_RETURN_VAL if empty.
+     */
     public int top() {
         if (isEmpty())
             return ERROR_RETURN_VAL;
         return last.value;
     }
 
+    /**
+     * @return
+     * This object removes the last Node element of the linked list.
+     * Returns ERROR_RETURN_VAL if empty.
+     */
     public int pop() {
         if (isEmpty())
             return ERROR_RETURN_VAL;
